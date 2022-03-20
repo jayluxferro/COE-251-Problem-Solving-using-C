02b9743e8f1357f0421faa241c946e0fd6541a3c
Jy <jayluxferro@sperixlabs.org>
2022-03-20 16:16:12 +0000

    added coding projects

---
+++ Lab/Session 2/Avg3Numbers/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+Name:           Justice Owusu Agyemang
+Index Number:   20532486
+Date:           11th March, 2022
+Task:           Find the average of 3 numbers.
+*/
+
+
+
+int main()
+{
+    int kofiNkansah = 13, amaBaby = 48, cushPutin = 99;
+
+    int sum = kofiNkansah + amaBaby + cushPutin;
+    double avg = sum/3.0;
+
+    printf("The average of %d, %d and %d is %.2f\n", kofiNkansah, amaBaby, cushPutin, avg);
+    return 0;
+}
